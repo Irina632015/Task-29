4d79d779764db79730950d04dbe29692f9426eef
Irina632015 <irina.632015@yandex.ru>
2023-01-29 19:20:13 +0400

    Скминар 4 задача 29

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+//*/
+int lenArray = ReadInt("Длинна массива: ");
+int[] randomArray = new int[lenArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+ randomArray[i] = new Random(). Next (1,32);
+    Console.Write(randomArray[i] + " ");
+}
+int ReadInt(string msg)
+{
+ Console.Write(msg);
+return Convert.ToInt32(Console.ReadLine());
+}
